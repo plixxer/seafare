182a204d1894ac9f5d56ab672f2fa80236aef0ef
Richard Grant <rg@ariesconsultinggroup.com>
2018-04-06 20:41:06 -0400

    a

--- updateattendee.php
+++ updateattendee.php
@@ -14,13 +14,13 @@
 
 	$t = $db->getall("salesforce.attendee__c",
 		Array(
-			"attendee_type__c"=> $_POST["data"]["attendeetype"],
-			"company__c"=> $_POST["account_id"],
-			"country__c"=> $_POST["data"]["country"],
-			"email__c"=> $_POST["data"]["email"],
-			"first_name__c"=> $_POST["data"]["firstname"],
-			"last_name__c"=> $_POST["data"]["lastname"],
-			"position__c"=> $_POST["data"]["position"]
+			"attendee_type__c"
+			"company__c"
+			"country__c"
+			"email__c"
+			"first_name__c"
+			"last_name__c"
+			"position__c"
 		),
 		Array(
 			'id'=>$_GET['sql_id']
