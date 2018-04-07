55af068a5b1ccce73e0f42fa9cda68149e29c392
Richard Grant <rg@ariesconsultinggroup.com>
2018-04-07 13:33:28 -0400

    a

--- guests.php
+++ guests.php
@@ -12,36 +12,36 @@ error_reporting(E_ALL ^ E_NOTICE);
 //attendee__c
 
 
-  $db = new _database(0x0);
+	$db = new _database(0x0);
 
-  $optional= Array();
-  if(!empty($_POST['orderby']) && !empty($_POST['order'])){
-    $optional = Array(
-      "ascdesc"=>Array($_POST['orderby'], $_POST['order'])
-    );
-  }
-  $optional['advanced'] = true;
+	$optional= Array();
+	if(!empty($_POST['orderby']) && !empty($_POST['order'])){
+		$optional = Array(
+			"ascdesc"=>Array($_POST['orderby'], $_POST['order'])
+		);
+	}
+	$optional['advanced'] = true;
 
-  $guest_data = $db->getall('salesforce.attendee__c',
-    Array(
-      "sfid",
-      "attendee_type__c",
-      "company__c",
-      "confirmation_number__c",
-      "country__c",
-      "email__c",
-      "first_name__c",
-      "last_name__c",
-      "position__c",
-      "remove__c"
-    ),
-    Array(
-      Array("company__c","=",$_POST['account_id']),
-      Array("attendee_type__c","=",'Guest'),
-      Array("remove__c","=",false),
-      Array("_hc_lastop","!=",'FAILED')
-    ),
-    $options
-  );
-  print_r(json_encode($guest_data, true));
+	$guest_data = $db->getall('salesforce.attendee__c',
+		Array(
+			"sfid",
+			"attendee_type__c",
+			"company__c",
+			"confirmation_number__c",
+			"country__c",
+			"email__c",
+			"first_name__c",
+			"last_name__c",
+			"position__c",
+			"remove__c"
+		),
+		Array(
+			Array("company__c","=",$_POST['account_id']),
+			Array("attendee_type__c","=",'Guest'),
+			Array("remove__c","=",false),
+			Array("_hc_lastop","!=",'FAILED')
+		),
+		$optional
+	);
+	print_r(json_encode($guest_data, true));
 ?>
